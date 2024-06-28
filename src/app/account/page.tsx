415ae5a158d51142d5f819d5c5cca23e2429ce4a
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-28 23:24:41 +0000

    added account page UI

---
+++ src/app/account/page.tsx
@@ -0,0 +1,26 @@
+import Form from '@/components/Form'
+
+type Props = {}
+
+const Account = (props: Props) => {
+  return (
+    <main className='flex min-h-[100dvh] bg-slate-100 justify-center items-center'>
+      <div className='w-fit'>
+        <div className="card bg-base-100 shadow-xl">
+          <div className='card-title justify-center'>
+            <h1 className='text-center'>ICON</h1>
+          </div>
+          <div className="card-body w-[480px] gap-5">
+            <div className='text-center'>
+              <h2 className=' font-semibold text-3xl '>Welcome Back</h2>
+              <p className='text-slate-500'>Welcome Back, Please Enter your details</p>
+            </div>
+            <Form />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Account
