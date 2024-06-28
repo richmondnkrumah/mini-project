7bff285591e8c4aab3110f04e4c528cd51463b54
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-28 15:18:01 +0000

    scraper Class

---
+++ src/utils/scraper.ts
@@ -0,0 +1,17 @@
+
+
+class WebScraper {
+  jobName: string
+  Location: string
+  
+  constructor(jobName: string,Location: string) {
+    this.jobName = jobName
+    this.Location = Location
+  }
+  async IndeedSite() {
+    
+  }
+}
+
+
+module.exports = WebScraper
