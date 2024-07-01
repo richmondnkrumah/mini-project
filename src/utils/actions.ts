fc37c2be63b060ee4d08be0552ea9b1cfb17c0cc
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-01 00:37:46 +0000

    added github supabase oauth

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -23,6 +23,17 @@ export const authWithGoogle = async () => {
 }
 
 export const authWithGithub = async () => {
+  const supabase = createClient()
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: 'http://example.com/auth/callback',
+    },
+  })
+  
+  if (data.url) {
+    redirect(data.url) // use the redirect API for your server framework
+  }
 
 }
 
