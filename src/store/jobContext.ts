bb1b7cc50aae930f60ea983e60035b63b01430f3
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-02 03:20:11 +0000

    added clear results func

--- src/store/jobContext.ts
+++ src/store/jobContext.ts
@@ -5,10 +5,12 @@ import { JOB } from "@/types/job";
 interface JobState {
   jobs?: JOB[],
   setNewJobResults: (jobResults: any) => void  
+  clearCurrentJobs: () => void
   
 }
 
 export const useJobStore = create<JobState>()((set) => ({
   jobs: undefined,
-  setNewJobResults: (jobResults: any) => set(state => ({ jobs: jobResults}))
+  setNewJobResults: (jobResults: any) => set(state => ({ jobs: jobResults})),
+  clearCurrentJobs: () => set({jobs: undefined})
 }));
