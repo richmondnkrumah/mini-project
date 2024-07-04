c1ff78056a7784c29bbee8a648afa1a5d4790d59
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-04 15:58:00 +0000

    ui fixes

--- src/components/JobDetail.tsx
+++ src/components/JobDetail.tsx
@@ -4,11 +4,11 @@ const JobDetail = ({ ActiveJob, isModal }: {ActiveJob?: JOB,isModal: boolean} ) 
   if (!ActiveJob) return null;
 
   return (
-    <div className={`ActiveJob-detail p-4 max-w-[900px] ${isModal && "hidden md:block"}`}>
+    <div className={`ActiveJob-detail p-4 max-w-[900px] ${isModal && "hidden md:block border-2 border-purple-500"}`}>
       <h2 className="text-xl font-bold mb-2">{ActiveJob.title}</h2>
       <p className="mb-2">{ActiveJob.company}</p>
       <div> {ActiveJob.summary} </div>
-      <p className="text-gray-500">Posted {ActiveJob.datePosted}</p>
+      <p className="text-gray-500">{ActiveJob.datePosted}</p>
     </div>
   );
 };
