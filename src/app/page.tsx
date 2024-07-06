4d4eb547c94a8fbf9c84ac7e96f6d330d40f9dde
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-06 02:14:51 +0000

    added custom loading animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ const Index = (props: Props) => {
       <Header isLandingPage={true} />
       <main className='grow pt-20 flex flex-col gap-20'>
         <div className='text-center flex flex-col items-center gap-8 '>
-          <h1 className='text-7xl'>Unlock Your <b className='text-purple-500'>Career potential</b> <br></br>with NicheJobs</h1>
-          <Link className='h-14 px-8 rounded-lg bg-purple-500 w-fit flex items-center text-white ' href="/home">Get Started</Link>
+          <h1 className='text-7xl'>Unlock Your <b className='text-[#660066]'>Career potential</b> <br></br>with NicheJobs</h1>
+          <Link className='h-14 px-8 rounded-lg bg-[#660066] w-fit flex items-center text-white ' href="/home">Get Started</Link>
         </div>
 
       </main>
---
+++ src/components/ContentLoading.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type Props = {}
+
+const ContentLoading = (props: Props) => {
+  return (
+    <div className="loader">
+    <div className="box box-1 ">
+      <div className="side-left"></div>
+      <div className="side-right"></div>
+      <div className="side-top"></div>
+    </div>
+    <div className="box box-2">
+      <div className="side-left"></div>
+      <div className="side-right"></div>
+      <div className="side-top"></div>
+    </div>
+    <div className="box box-3">
+      <div className="side-left"></div>
+      <div className="side-right"></div>
+      <div className="side-top"></div>
+    </div>
+    <div className="box box-4">
+      <div className="side-left"></div>
+      <div className="side-right"></div>
+      <div className="side-top"></div>
+    </div>
+  </div>
+  )
+}
+
+export default ContentLoading
