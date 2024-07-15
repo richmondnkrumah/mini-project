10324b1ecb5abcfb1362c7a601c1f8078a756699
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-15 19:58:03 +0000

    added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {}
+
+const AboutPage = (props: Props) => {
+  return (
+    <>
+    <Header isLandingPage={true} />
+    <main className='grow'>
+      
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default AboutPage
