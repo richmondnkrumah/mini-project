5f130d93a026b3390e9bb84a5320f7304a4d1947
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-07-19 13:50:09 +0000

    added remote check functon

--- src/store/jobContext.ts
+++ src/store/jobContext.ts
@@ -4,13 +4,17 @@ import { JOB } from "@/types/job";
 
 interface JobState {
   jobs?: JOB[],
+  rawJobs?: JOB[],
   setNewJobResults: (jobResults: any) => void  
   clearCurrentJobs: () => void
+  setRemoteOnlyJobs: (shouldFilter: boolean) => void
   
 }
 
 export const useJobStore = create<JobState>()((set) => ({
   jobs: undefined,
-  setNewJobResults: (jobResults: any) => set(state => ({ jobs: jobResults})),
-  clearCurrentJobs: () => set({jobs: undefined})
+  rawJobs: undefined,
+  setNewJobResults: (jobResults: any) => set({ jobs: jobResults, rawJobs: jobResults}),
+  clearCurrentJobs: () => set({jobs: undefined}),
+  setRemoteOnlyJobs: (shouldFilter) => set(state => ({jobs: shouldFilter ? state.jobs?.filter(currJob => currJob.isRemote) : state.rawJobs}))
 }));
