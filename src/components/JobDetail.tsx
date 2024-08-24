7621dfa0ccab21efddb0dca86f93d7e0658db77b
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-08-24 02:05:20 +0000

    fixed link and ui issues

--- src/components/JobDetail.tsx
+++ src/components/JobDetail.tsx
@@ -5,6 +5,9 @@ import SalarySVG from '@/assets/salary.svg'
 import LocationSVG from '@/assets/location.svg'
 import BookmarkSVG from '@/assets/bookmark.svg'
 import Image from 'next/image';
+import ContentLoading from './ContentLoading';
+import ServerDownIllustration from '@/assets/serverdown.svg'
+
 
 
 const JobDetail = ({ ActiveJob }: { ActiveJob?: JOB }) => {
@@ -16,7 +19,7 @@ const JobDetail = ({ ActiveJob }: { ActiveJob?: JOB }) => {
 
   useEffect(() => {
     const getJobDetails = async (jobUrl: string) => {
-       
+
       const formData = new FormData()
       formData.set("jobUrl", jobUrl)
 
@@ -34,7 +37,7 @@ const JobDetail = ({ ActiveJob }: { ActiveJob?: JOB }) => {
         else {
           setCurrentJobDetails(results.data)
         }
-         
+
       } catch (error) {
         // Handle error
         setErrorJobResults("Error Submitting Form")
@@ -49,33 +52,43 @@ const JobDetail = ({ ActiveJob }: { ActiveJob?: JOB }) => {
 
   }, [ActiveJob])
 
+  console.log("This is the apply link baby girls", currentJobDetails)
 
   return (
     <div className={`ActiveJob-detail p-4 h-[96dvh] min-w-[50%] "hidden md:block border-2 border-[#660066] rounded-lg sticky top-[1rem] " overflow-auto`}>
       {
-        loadingJobResults && <div>Loading</div>
+        loadingJobResults &&
+        <div className="flex flex-col gap-10 items-center justify-center h-full">
+          <ContentLoading />
+          <p className="text-lg">Getting Additional Job details from Scraper</p>
+
+        </div>
       }
       {
-        errorJobResults && <div>Error</div>
+        errorJobResults &&
+        <div className=" flex flex-col items-center justify-center h-full">
+          <Image className="max-h-[80%] max-w-[80%]" src={ServerDownIllustration} alt="Error from Server" />
+          <p className="text-2xl">The Scraper ran into an Error</p>
+          <p className="text-2xl text-[#660066]">Please Try Again</p>
+        </div>
       }
       {
         (!loadingJobResults && !errorJobResults) &&
         <div>
           <div className=' flex flex-col gap-1'>
-            <h2 className="text-2xl font-bold mb-2">{currentJobDetails?.title}</h2>
-                <a className='flex gap-1 underline' href={currentJobDetails?.companyLink}>{currentJobDetails?.companyName}
+            <h2 className="text-3xl font-bold">{currentJobDetails?.title}</h2>
+            <a target='_blank' className='flex gap-1 underline' href={currentJobDetails?.companyLink}>{currentJobDetails?.companyName.split('.')[0]}
 
-                <Image src={LinkSVG} width={20} height={20} alt='linker svg' />
-                </a>
+              <Image src={LinkSVG} width={20} height={20} alt='linker svg' />
+            </a>
           </div>
-          <div className='flex gap-3'>
-            <button className='flex gap-1 px-6 py-3 justify-center bg-red-300'>
-              Apply
-              <Image src={LinkSVG} height={15} width={15} alt='link svg' />
+          <div className='flex gap-5 my-3'>
+            <button className='flex gap-1 px-6 py-3 justify-center items-center bg-[#660066] rounded-xl'>
+              <a target='_blank' href={currentJobDetails?.applyLink} className='text-white'> Apply</a>
+              <Image src={LinkSVG} height={15} width={15} alt='link svg' className='invert'/>
             </button>
-            <button className='flex gap-1 px-6 py-3'>
-              Bookmark
-              <Image src={BookmarkSVG} height={25} width={25} alt='Bookmark svg' />
+            <button className='flex gap-1 px-4 py-2 items-center bg-[#660066] rounded-xl'>
+              <Image src={BookmarkSVG} className=' invert' height={25} width={25} alt='Bookmark svg' />
 
             </button>
           </div>
@@ -98,7 +111,7 @@ const JobDetail = ({ ActiveJob }: { ActiveJob?: JOB }) => {
           </div>
           <div className='mt-4'>
             <h3 className='text-xl font-bold'>Full Job Description</h3>
-            <p>{currentJobDetails?.description}</p>
+            <p className=' whitespace-pre-line'>{currentJobDetails?.description.replaceAll(/(\n\s+)+/g,"\n").replace("\n","")}</p>
           </div>
         </div>
       }
